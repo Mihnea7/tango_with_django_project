6c26862e14c17592d29ba3312190de3530d370d8
Mihnea Maldaianu <2289803m@student.gla.ac.uk>
2019-02-07 01:05:53 +0000

    end of chapter 9

--- rango/models.py
+++ rango/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views=models.IntegerField(default=0)
@@ -27,3 +27,10 @@ class Page(models.Model):
     def __str__(self): # For Python 2, use __unicode__ too
         return self.title
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to = 'profile_images', blank = True)
+
+    def __str__(self):
+        return self.user.username
